be39f54f7d435ea4a0e40518887812aa3ce174b8
Anastasia Guschina <agushhina@appline.ru>
2021-05-26 23:51:14 +0300

    1 задание. Написана первая программа на java

---
+++ src/TaskWork01.java
@@ -0,0 +1,6 @@
+public class TaskWork01 {
+    //Написать первую программу, которая выводит в консоль строку “Hello World!”
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
